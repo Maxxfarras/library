522bc5a5b48e4b764698722f8afe714de1372b51
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2024-12-02 21:16:06 -0700

    add show and hide dialog in js
    format both documents
    
    reorganize labels in form
    
    need to work on form bugs

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let buttonBook = document.querySelector('#book-button')
-let buttonSubmit = document.querySelector('#submit')
-let buttonCancel = document.querySelector('#cancel')
-let form = document.querySelector('#book-form')
-let dialog = document.querySelector('dialog')
+let buttonBook = document.querySelector("#book-button");
+let buttonCancel = document.querySelector("#cancel");
+let form = document.querySelector("#book-form");
+let dialog = document.querySelector("dialog");
 let mainLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -30,15 +29,22 @@ function addBookToLibrary() {
   book = {};
 }
 
-buttonBook.addEventListener('click', () => {
-   dialog.showModal(); 
-})
+buttonBook.addEventListener("click", () => {
+  dialog.showModal();
+});
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let formData = new FormData(form)
-    let title = formData.get('title-book')
-    let author = formData.get('author-book')
-    let pages = formData.get('pages-book')
-    let isRead = formData.get('isRead-book')
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let formData = new FormData(form);
+  let title = formData.get("title-book");
+  let author = formData.get("author-book");
+  let pages = formData.get("pages-book");
+  let isRead = formData.get("isRead-book");
+  form.reset();
+  dialog.close();
+});
+
+buttonCancel.addEventListener("click", () => {
+  dialog.close();
+  form.reset();
+});
