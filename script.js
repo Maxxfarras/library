25ad8e6d84d8eef8e8223a9bad0045f12ce8e75a
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2024-12-02 18:43:16 -0700

    working on addBookToLibrary
    add validations on pages
    
    use the object constructor to create new objects
    
    seem to work fine

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 let mainLibrary = [];
 
 function Book(title, author, pages, isRead) {
-   this.title = title;
-   this.author = author;
-   this.pages = pages;
-   this.isRead = isRead; 
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
-let testBook1 = new Book('pedro paramo', 'juan rulfo', 69, true)
-
-let testBook2 = new Book('el llano en llamas', 'juan rulfo', 88, false)
-
-let testBook3 = new Book('rayuela', 'julio cortazar', 400, true)
-
-let testBook4 = new Book('el tunel', 'ernesto sabato', 89, true)
-
-
-function addBookToLibrary(bookObject) {
-    mainLibrary.push(bookObject)
+function addBookToLibrary() {
+  let title;
+  let author;
+  let pages;
+  let isRead;
+  title = prompt("Title:");
+  author = prompt("Author:");
+  pages = prompt("Pages:");
+  while (pages % 1 != 0 || pages < 1) {
+    alert("Should enter a correct value");
+    pages = prompt("Pages:");
+  }
+  isRead = confirm("Have you read it?");
+  let book = new Book(title, author, pages, isRead);
+  mainLibrary.push(book);
+  book = {};
 }
 
-addBookToLibrary(testBook1)
-addBookToLibrary(testBook2)
-addBookToLibrary(testBook3)
-addBookToLibrary(testBook4)
-
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+console.log(mainLibrary);
