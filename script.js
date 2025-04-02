550c52f9a346bf13646372e6bb4e2435759564b2
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2025-04-01 21:59:32 -0700

    script redone for classes basically from scratch to practice classes
    seems to work fine
    
    merge to main

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let buttonBook = document.querySelector("#book-button");
-let buttonCancel = document.querySelector("#cancel");
-let form = document.querySelector("#book-form");
-let dialog = document.querySelector("dialog");
-let bookCount = 0;
-
-/*
-function Book(title, author, pages, isRead, comment) {
-  //object constructor
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-  this.comment = comment;
-}
-*/
-
 class Book {
   constructor(title, author, pages, isRead, comment) {
     this.title = title;
@@ -42,61 +25,82 @@ class Book {
   }
 }
 
-/*
-Book.prototype.buttonColorChange = document
-  .querySelector("#main-container") //checks whole container for the .isRead-button
-  .addEventListener("click", (event) => {
-    if (event.target.classList.contains("isRead-button")) {
-      const isReadButton = event.target; //when in click
-      if (isReadButton.textContent === "Read") {
-        //toggle color depending on the textContent
-        isReadButton.textContent = "Not Read";
-        isReadButton.style.backgroundColor = "#fafffd";
-      } else {
-        isReadButton.textContent = "Read";
-        isReadButton.style.backgroundColor = "#a2d729";
-      }
-    }
-  });
-  */
+class Library {
+  constructor() {
+    this.buttonBook = document.querySelector("#book-button");
+    this.dialog = document.querySelector("dialog");
+    this.bookCount = 0;
 
-//to avoid content overflowing, limit 9 cards
-buttonBook.addEventListener("click", () => {
-  if (bookCount < 9) {
-    dialog.showModal();
-  } else {
-    alert("You have reached the book limit, delete some books.");
+    this.buttonBook.addEventListener("click", () => this.newBookClickHandler());
+
+    document
+      .querySelector("#main-container")
+      .addEventListener("click", (event) => this.deleteCard(event, this));
   }
-});
 
-//event listener on submit dialog
-form.addEventListener("submit", (event) => {
-  event.preventDefault(); //prevent server connection
-  let formData = new FormData(form);
-  let title = formData.get("title-book");
-  let author = formData.get("author-book");
-  let pages = formData.get("pages-book");
-  let isRead = formData.get("isRead-book");
-  let comment = formData.get("comment-book");
-  isRead === null ? (isRead = "Not Read") : (isRead = "Read"); //to avoid null
-  let book = new Book(title, author, pages, isRead, comment);
-  createCard(book);
-  bookCount += 1;
-  form.reset();
-  dialog.close();
-});
+  newBookClickHandler() {
+    if (this.bookCount < 9) {
+      this.dialog.showModal();
+    } else {
+      alert("You have reached the book limit, delete some books.");
+    }
+  }
 
-//event listener to cancel dialog
-buttonCancel.addEventListener("click", () => {
-  dialog.close();
-  form.reset();
-});
+  deleteCard(event, obj) {
+    if (event.target.classList.contains("delete-button")) {
+      //checks the whole container for divs with .delete-button
+      let card = event.target.closest(".book-card"); //selects the nearest ancestor
+      card.remove();
+      obj.bookCount -= 1;
+    }
+  }
 
-function createCard(book) {
-  let container = document.querySelector("#main-container");
-  let bookCard = document.createElement("div");
-  bookCard.classList.toggle("book-card");
-  bookCard.innerHTML = `
+  addBookCount() {
+    this.bookCount += 1
+  }
+
+  checkBookCount() {
+    return this.bookCount
+  }
+
+}
+
+class Form {
+  constructor() {
+    this.form = document.querySelector("#book-form");
+    this.dialog = document.querySelector("dialog");
+    this.buttonCancel = document.querySelector("#cancel");
+    this.bookCount = 0;
+    this.form.addEventListener("submit", (event) => this.formSubmitHandler(event, this));
+    this.buttonCancel.addEventListener("click", () => this.cancelClickHandler());
+  }
+
+  formSubmitHandler(event, obj) {
+    event.preventDefault(); //prevent server connection
+    let formData = new FormData(this.form);
+    let title = formData.get("title-book");
+    let author = formData.get("author-book");
+    let pages = formData.get("pages-book");
+    let isRead = formData.get("isRead-book");
+    let comment = formData.get("comment-book");
+    isRead === null ? (isRead = "Not Read") : (isRead = "Read"); //to avoid null
+    let book = new Book(title, author, pages, isRead, comment);
+    obj.createCard(book);
+    myLibrary.addBookCount();
+    obj.form.reset();
+    obj.dialog.close();
+  }
+
+  cancelClickHandler() {
+    this.dialog.close();
+    this.form.reset();
+  }
+
+  createCard(book) {
+    let container = document.querySelector("#main-container");
+    let bookCard = document.createElement("div");
+    bookCard.classList.toggle("book-card");
+    bookCard.innerHTML = `
   <div>Title: ${book.title}</div>
   <div>Author: ${book.author}</div>
   <div>Pages: ${book.pages}</div>
@@ -104,21 +108,16 @@ function createCard(book) {
   <button class='isRead-button'>${book.isRead}</button>
   <button class='delete-button'>Delete</button>
   `; //Create card based on book data
-  container.appendChild(bookCard);
-  let isReadButton = bookCard.querySelector(".isRead-button"); //sets default color on the isRead-button
-  if (book.isRead === "Read") {
-    isReadButton.style.backgroundColor = "#a2d729";
-  } else {
-    isReadButton.style.backgroundColor = "#fafffd";
+    container.appendChild(bookCard);
+    let isReadButton = bookCard.querySelector(".isRead-button"); //sets default color on the isRead-button
+    if (book.isRead === "Read") {
+      isReadButton.style.backgroundColor = "#a2d729";
+    } else {
+      isReadButton.style.backgroundColor = "#fafffd";
+    }
   }
 }
 
-//event listeners for delete button
-document.querySelector("#main-container").addEventListener("click", (event) => {
-  if (event.target.classList.contains("delete-button")) {
-    //checks the whole container for divs with .delete-button
-    let card = event.target.closest(".book-card"); //selects the nearest ancestor
-    card.remove();
-    bookCount -= 1;
-  }
-});
+let myLibrary = new Library();
+
+let myForm = new Form()
